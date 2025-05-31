d30bda6f10d50b066fa67821d1257696bce06e2d
Oleksandr-Berd <alex.berd86@gmail.com>
2025-05-31 09:45:10 +0300

    laptop section is done

--- src/components/Laptop/Laptop.jsx
+++ src/components/Laptop/Laptop.jsx
@@ -1,13 +1,18 @@
 import * as SC from "./LaptopStyled"
 
 import laptopMob from "../../assets/images/illustration-laptop-mobile.svg"
+import laptopDesk from "../../assets/images/illustration-laptop-desktop.svg"
+import { useScreenSize } from "../../hooks/useScreenSize"
 const Laptop = () => {
+
+    const {isDesktop} = useScreenSize()
+
     return ( 
         <SC.LaptopCustom>
             <SC.Thumb>
-            <img src={laptopMob} alt="laptop" />
+            <img src={!isDesktop ? laptopMob : laptopDesk} alt="laptop" />
             </SC.Thumb>
-            <div>
+            <SC.ArticleCon>
                 <SC.Article>
                     <h3>Free, open, simple</h3>
                     <p>Blogr is a free and open source application backed by a large community of helpful developers. It supports features such as code syntax highlighting, RSS feeds, social media integration, third-party commenting tools, and works seamlessly with Google Analytics. The architecture is clean and is relatively easy to learn.</p>
@@ -16,7 +21,7 @@ const Laptop = () => {
                     <h3>Powerful tooling</h3>
                     <p>Batteries included. We built a simple and straightforward CLI tool that makes customization and deployment a breeze, but capable of producing even the most complicated sites.</p>
                 </SC.Article>
-            </div>
+            </SC.ArticleCon>
         </SC.LaptopCustom>
      );
 }
--- src/components/Laptop/LaptopStyled.js
+++ src/components/Laptop/LaptopStyled.js
@@ -6,17 +6,50 @@ export const LaptopCustom = styled.div`
   padding-bottom: 100px;
 
   text-align: center;
+
+  @media (min-width: 1280px) {
+    position: relative;
+
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+
+    padding-left: 0;
+    padding-right: 164px;
+    padding-top: 120px;
+    padding-bottom: 240px;
+  }
 `;
 
 export const Thumb = styled.div`
   margin-bottom: 46px;
+
+  @media (min-width: 1280px) {
+    position: absolute;
+    top: 0;
+    left: 0;
+
+    transform: translate(-36%, -56px);
+
+    margin: 0;
+  }
+`;
+
+export const ArticleCon = styled.div`
+  @media (min-width: 1280px) {
+    width: 42%;
+
+    text-align: left;
+  }
 `;
 
 export const Article = styled.div`
+  @media (min-width: 120px) {
+  }
 
-&:first-of-type{
+  &:first-of-type {
     margin-bottom: 40px;
-}
+  }
 
   & > h3 {
     margin-bottom: 16px;
@@ -33,6 +66,8 @@ export const Article = styled.div`
     font-weight: 400;
     line-height: 1.75;
     letter-spacing: 0.5px;
-    color: #4C5862;
+    color: #4c5862;
+
+    opacity: 0.7;
   }
 `;
